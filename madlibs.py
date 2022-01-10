4453c316c3f20d309c1e972945f5b71108c75694
Marcelo Cortez <marcelohcortez@gmail.com>
2022-01-10 21:31:21 +0000

    Add main file and the first Mad Lib

---
+++ madlibs.py
@@ -0,0 +1,6 @@
+from samples import old_macdonald
+import random
+
+if __name__ == "__main__":
+    m = random.choice([old_macdonald])
+    m.madlib()
---
+++ samples/old_macdonald.py
@@ -0,0 +1,15 @@
+def madlib():
+    adjective = input("Adjective: ")
+    noun = input("Noun: ")
+    animal = input("Animal: ")
+    noise = input("Noise: ")
+
+    madlib = f"{adjective} Macdonald had a {noun}, E-I-E-I-O \
+    and on that {noun} he had an {animal}, E-I-E-I-O \
+    with a {noise} {noise} here \
+    and a {noise} {noise} there, \
+    here a {noise}, there a {noise}, \
+    everywhere a {noise} {noise}, \
+    {adjective} Macdonald had a {noun}, E-I-E-I-O."
+
+    print(madlib)
